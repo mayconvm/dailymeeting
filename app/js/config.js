4f544fa90624e7dd97d3eb5415e4db3420eced91
MayconVM <mayconvm@gmail.com>
2017-10-26 23:34:05 -0200

    Fluxo para validação e login do usuário.

--- app/js/config.js
+++ app/js/config.js
@@ -1,23 +1,96 @@
 (function (_global) {
   
   const user = new User();
+  const sendData = new SendData();
 
-  if (user.getName() != "") {
-    return window.location.href = "popup.html";
+  if (user.getName()) {
+    return console.log("Logado", user);
+    // return window.location.href = "popup.html";
   }
 
-  let sendForm = document.getElementById("sendForm");
-  // register click
-  sendForm.addEventListener('click', sendDataForm);
+  // flow
+  let areaStep1 = document.getElementById("step1");
+  let areaStep2 = document.getElementById("step2");
 
-  function sendDataForm() {
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
+  ///////////
+  // STEP1 //
+  ///////////
 
-    user.setName(name);
-    user.setEmail(email);
+  let sendFormStep1 = document.getElementById("sendFormStep1");
+  sendFormStep1.addEventListener('click', function() {
+    // notify user that loading
+    changeButton(this);
+    user.setEmail(getValueInput("email"));
+    user.setPassword(getValueInput("password"));
 
-    window.location.reload();
+    sendData.setEntity(user);
+    sendData.send().then((data) => {
+      // case success
+      // hide area #step1
+      areaStep1.classList.add("d-none");
+      
+      // show area #step2
+      areaStep2.classList.remove("d-none");
+
+      // token
+      user.setToken(data.token);
+    })
+    .catch((dataError) => {
+      // case email not found
+      // show message error
+      alert(JSON.stringify(dataError));
+    });
+      
+  });
+
+  ////////////
+  // STEP 2 //
+  ////////////
+
+  let sendFormStep2 = document.getElementById("sendFormStep2");
+  sendFormStep2.addEventListener('click', function() {
+    // notify uset that loading
+    changeButton(this);
+    user.setName(getValueInput("name"));
+    user.setTeam(getValueInput("email"));
+    user.setPassword(getValueInput("password"));
+
+    sendData.setEntity(user);
+    sendData.send().then((data) => {
+      // case success
+      // registre new password
+      // persist data about user
+      // redirect user to popup.html
+      window.location.reload();
+    })
+    .catch((dataError) => {
+      // case error
+      // show message error
+      alert(JSON.stringify(dataError));
+    });
+  });
+
+  function changeButton(el) {
+    if (!el && !('innerHTML' in el)) {
+      return;
+    }
+
+    let dataText = el.innerHTML;
+    let image = '<img class="loader" src="img/tail-spin.svg" alt="Loader"> ';
+
+    if (dataText.indexOf(image) === -1) {
+      dataText = image + dataText;
+      el.disabled = true;
+    } else {
+      dataText = dataText.replace(image, '');
+      el.disabled = false;
+    }
+    
+    el.innerHTML =  dataText;
+  }
+
+  function getValueInput(name) {
+    return document.getElementById(name).value;
   }
 
 }) (window);
